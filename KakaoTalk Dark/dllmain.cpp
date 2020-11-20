51d7df87068d5941adae19e2b0d8da88efcf5c41
unknown <newjincar33@gmail.com>
2020-11-20 18:49:54 +0900

    이미지 로드 함수 후킹

--- KakaoTalk Dark/dllmain.cpp
+++ KakaoTalk Dark/dllmain.cpp
@@ -1,12 +1,19 @@
 ﻿#include "pch.h"
 using namespace Gdiplus;
+using namespace Gdiplus::DllExports;
 
 typedef HBRUSH(WINAPI* ORGFP1) (COLORREF);
 typedef COLORREF(WINAPI* ORGFP2) (HDC, COLORREF);
-typedef GpStatus(WINGDIPAPI* ORGFP3) (GpImage*, GUID*);
+typedef GpStatus(WINGDIPAPI* ORGFP3) (GDIPCONST WCHAR* filename, GpBitmap** bitmap);
+typedef GpStatus(WINGDIPAPI* ORGFP4) (GDIPCONST WCHAR* filename, GpBitmap** bitmap);
+typedef GpStatus(WINGDIPAPI* ORGFP5) (IStream* stream, GpBitmap** bitmap);
+typedef GpStatus(WINGDIPAPI* ORGFP6) (IStream* stream, GpBitmap** bitmap);
 ORGFP1 originFunc1 = CreateSolidBrush;
 ORGFP2 originFunc2 = SetTextColor;
-ORGFP3 originFunc3 = DllExports::GdipGetImageRawFormat;
+ORGFP3 originFunc3 = GdipCreateBitmapFromFile;
+ORGFP4 originFunc4 = GdipCreateBitmapFromFileICM;
+ORGFP5 originFunc5 = GdipCreateBitmapFromStream;
+ORGFP6 originFunc6 = GdipCreateBitmapFromStreamICM;
 
 DWORD WriteLog(LPCTSTR lpszFormat, ...) {
 	TCHAR szLog[512];
@@ -34,9 +41,24 @@ COLORREF WINAPI DetourFunc2(HDC hdc, COLORREF color) {
 	return originFunc2(hdc, color);
 }
 
-GpStatus WINGDIPAPI DetourFunc3(GpImage* image, GUID* format) {
-	WriteLog(TEXT("GdipGetImageRawFormat(0x%08X, 0x%08X)\n"), image, format);
-	return originFunc3(image, format);
+GpStatus WINGDIPAPI DetourFunc3(GDIPCONST WCHAR* filename, GpBitmap** bitmap) {
+	WriteLog(TEXT("GdipCreateBitmapFromFile(%s, 0x%08X)\n"), filename, bitmap);
+	return originFunc3(filename, bitmap);
+}
+
+GpStatus WINGDIPAPI DetourFunc4(GDIPCONST WCHAR* filename, GpBitmap** bitmap) {
+	WriteLog(TEXT("GdipCreateBitmapFromFileICM(%s, 0x%08X)\n"), filename, bitmap);
+	return originFunc4(filename, bitmap);
+}
+
+GpStatus WINGDIPAPI DetourFunc5(IStream* stream, GpBitmap** bitmap) {
+	WriteLog(TEXT("GdipCreateBitmapFromStream(0x%08X, 0x%08X)\n"), stream, bitmap);
+	return originFunc5(stream, bitmap);
+}
+
+GpStatus WINGDIPAPI DetourFunc6(IStream* stream, GpBitmap** bitmap) {
+	WriteLog(TEXT("GdipCreateBitmapFromStreamICM(0x%08X, 0x%08X)\n"), stream, bitmap);
+	return originFunc6(stream, bitmap);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
@@ -52,6 +74,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 		DetourAttach(&(PVOID&)originFunc1, DetourFunc1);
 		DetourAttach(&(PVOID&)originFunc2, DetourFunc2);
 		DetourAttach(&(PVOID&)originFunc3, DetourFunc3);
+		DetourAttach(&(PVOID&)originFunc4, DetourFunc4);
+		DetourAttach(&(PVOID&)originFunc5, DetourFunc5);
+		DetourAttach(&(PVOID&)originFunc6, DetourFunc6);
 		DetourTransactionCommit();
 		break;
 	case DLL_PROCESS_DETACH:
@@ -61,6 +86,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 		DetourDetach(&(PVOID&)originFunc1, DetourFunc1);
 		DetourDetach(&(PVOID&)originFunc2, DetourFunc2);
 		DetourDetach(&(PVOID&)originFunc3, DetourFunc3);
+		DetourDetach(&(PVOID&)originFunc4, DetourFunc4);
+		DetourDetach(&(PVOID&)originFunc5, DetourFunc5);
+		DetourDetach(&(PVOID&)originFunc6, DetourFunc6);
 		DetourTransactionCommit();
 		break;
 	}
